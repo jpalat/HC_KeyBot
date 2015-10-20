bbf15602b5ebd3226ab82a804aba7fe0055e6266
Jay Palat <jay@palat.net>
2015-10-20 00:27:44 -0400

    fixed bad prepared statement

--- main.go
+++ main.go
@@ -191,7 +191,7 @@ func (c *Context) set_keys(w http.ResponseWriter, r *http.Request) {
 			userID:  senderID,
 		}
 
-		stmt, err := db.Prepare("INSERT INTO keys(userid, keytype, keytext) VALUES(?,?,?)")
+		stmt, err := db.Prepare("INSERT INTO keys(userid, keytype, keytext) VALUES($1,$2,$3:wq)")
 		if err != nil {
 			log.Fatal(err)
 		}
